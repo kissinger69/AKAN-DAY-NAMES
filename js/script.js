fd73fbd27b42b96a166469757bb61ec378e5d4ba
kissinger69 <kissyoa69@gmail.com>
2020-04-02 17:31:06 +0300

    AKANDAYNAMES

--- js/script.js
+++ js/script.js
@@ -1,124 +1,74 @@
-function akanDayNames() {
-   var daysoftheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-   var maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-   var male = parseInt(document.getElementById("male").value);
-   var femaleAkanNAmes = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-   var female = parseInt(document.getElementById("female").value);
-   var day = parseInt(document.getElementById("day").value);
-   var DD = parseInt(day);
-   var month = parseInt(document.getElementById("month").value);
-   var MM = parseInt(month);
-   var year = parseInt(document.getElementById("year").value);
-   var YY =  parseInt(year);
-}
+function akanNames () {
+    var year = document.getElementById("year").value;
+    var YY =  parseInt(year);
+    var month = document.getElementById("month").value;
+    var MM = parseInt(month);
+    var day = document.getElementById("day").value;
+    var DD = parseInt(day);
+    var CC = (YY - 1) / 100 + 1;
+    var maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    var femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+   
+   //Formula to derive Akan Names.
+    var final = parseInt(CC / 4 - 2 * CC - 1 + ( 5 * YY) / 4 + ( 26 * (MM + 1)) / 10 + DD) % 7;
 
-   var sex;
+   
    //sex
-   if (document.getElementById("sex").checked) {
-     sex = 'male';
-   } else {
-       sex = 'female';
-  } 
-
-//Both incoorect and correct date inform user.
-  if (DD >= 1 &&  DD <= 31) {
-    alert("Good progress");
-}   else (DD < 1 || DD > 31) 
-      alert("INVALID DATE!");
-
-//Confirming months.
-  if (MM >= 1  && MM <= 12) {
-    alert("Good progress.");
-}     else if (MM ==2 && DD > 29); {
-         alert("February doesnt have those number of days");
-}  
-   if (MM < 0 || MM > 12) {
-      alert("INVALID MONTH!");
- } 
-
-   //Confirming year.
-    if (YY < 1000 && YY > 2020) {
-      alert("Good Progress");
-    }
-
-    //maleAkanNames
-    //Sunday
-    if (Math.ceil(daysoftheWeek) === 0 && sex ==='male') {
-        document.getElementById("results").innerHTML = 
-         console.log("Your Akan Day Name is" + maleAkanNames[0]);
-    }
-    //Monday
-    else if (Math.ceil(daysoftheWeek) ===1 && sex ==='male') {
-           document.getElementById("results").innerHTML =
-            console.log ("Your Akan Day Name is" + maleAkanNames[1]);
-    }
-    //Tuesday
-    else if (Math.ceil(daysoftheWeek)===2 && sex === 'male') {
-          document.getElementById("results").innerHTML =
-            console.log("Your Akan Day Name is" + maleAkanNames[2]);
-    }
-    //Wednesday
-    else if (Math.ceil(daysoftheWeek) ===3 && sex ==='male') {
-      document.getElementById("results").innerHTML =
-        console.log("Your Akan Day Name is" + maleAkanNames[3]);
-    }
-    //Thursday
-    else if (Math.ceil(daysoftheWeek) ===4 && sex ==='male') {
-      document.getElementById("results").innerHTML =
-       console.log("Your Akan Day Name is" + maleAkanNames[4]);
-    }
-    //Friday
-    else if (Math.ceil(daysoftheWeek) ===5 && sex ==='male') {
-      document.getElementById("results").innerHTML =
-        console.log("Your Akan Day Name is" + maleAkanNames[5]);
-    }
-     //Saturday
-     else if (Math.ceil(daysoftheWeek) ===6 && sex ==='male') {
-      document.getElementById("results").innerHTML =
-        console.log("Your Akan Day Name is" + maleAkanNames[6]);
+    if (document.getElementById("gender").checked) {
+      var gender = "male";
+    } else {
+        var gender = "female";
     } 
-    //FemaleAkanNames
-    //Sunday
-    if (Math.ceil(daysoftheWeek) === 0 && sex ==='female') {
-      document.getElementById("results").innerHTML = 
-       console.log("Your Akan Day Name is" + femaleAkanNames[0]);
-  }
-  //Monday
-  if (Math.ceil(daysoftheWeek) === 1 && sex ==='female') {
-    document.getElementById("results").innerHTML = 
-     console.log("Your Akan Day Name is" + femaleAkanNames[1]);
-}
-//Tuesday
-if (Math.ceil(daysoftheWeek) === 2 && sex ==='female') {
-  document.getElementById("results").innerHTML = 
-   console.log("Your Akan Day Name is" + femaleAkanNames[2]);
-}
-//Wednesday
-if (Math.ceil(daysoftheWeek) === 3 && sex ==='female') {
-  document.getElementById("results").innerHTML = 
-   console.log("Your Akan Day Name is" + femaleAkanNames[3]);
-}
-//Thursday
-if (Math.ceil(daysoftheWeek) === 4 && sex ==='female') {
-  document.getElementById("results").innerHTML = 
-   console.log("Your Akan Day Name is" + femaleAkanNames[4]);
-}
-//Friday
-if (Math.ceil(daysoftheWeek) === 5 && sex ==='female') {
-  document.getElementById("results").innerHTML = 
-   console.log("Your Akan Day Name is" + maleAkanNames[5]);
-}
-//Saturday
-if (Math.ceil(daysoftheWeek) === 6 && sex ==='female') {
-  document.getElementById("results").innerHTML = 
-   console.log("Your Akan Day Name is" + femaleAkanNames[6]);
-} else {
-   alert('Please enter your information')
-}
 
-
-
-
-
-    
-
+   if (DD < 1 || DD > 31) {  // incorrect date inform user.
+       alert("INVALID DATE!");
+   } else if (MM < 1 || MM > 12 || (MM == 2 && DD > 29)) {
+       alert("INVALID MONTH");
+   } else if (YY < 1000 && YY > 2020) {
+     alert("INVALID YEAR!");
+   } else if (Math.ceil(final) == 0 && gender === "male") {
+      document.getElementById("result").innerHTML = 
+        "You were born on Sunday,Your Akan Day Name is" + maleAkanName[0];
+   } else if (Math.ceil(final) == 1 && gender === "male") {
+      document.getElementById("result").innerHTML = 
+       "You were born on Monday,Your Akan Day Name is" + maleAkanName[1];
+   } else if (Math.ceil(final) == 2 && gender === "male") {
+    document.getElementById("result").innerHTML = 
+    "You were born on Tuesday,Your Akan Day Name is" + maleAkanName[2];
+  } else if (Math.ceil(final) == 3 && gender === "male") {
+   document.getElementById("result").innerHTML = 
+    "You were born on Wednesday,Your Akan Day Name is" + maleAkanName[3];
+  } else if (Math.ceil(final) == 4 && gender === "male") {
+     document.getElementById("result").innerHTML = 
+      "You were born on Thursday,Your Akan Day Name is" + maleAkanName[4];
+  }   else if (Math.ceil(final) == 5 && gender === "male") {
+   document.getElementById("result").innerHTML = 
+    "You were born on Friday,Your Akan Day Name is" + maleAkanName[5];
+  } else if (Math.ceil(final) == 6 && gender === "male") {
+     document.getElementById("result").innerHTML = 
+      "You were born on Saturday,Your Akan Day Name is" + maleAkanName[6];
+  } else if (Math.ceil(final) == 0 && gender === "female") {
+      document.getElementById("result").innerHTML = 
+        "You were born on Sunday,Your Akan Day Name is" + femaleAkanName[0];
+  } else if (Math.ceil(final) == 1 && gender === "female") {
+     document.getElementById("result").innerHTML = 
+      "You were born on Monday,Your Akan Day Name is" + femaleAkanName[1];
+  } else if (Math.ceil(final) == 2 && gender === "female") {
+     document.getElementById("result").innerHTML = 
+       "You were born on Tuesday,Your Akan Day Name is" + femaleAkanName[2];
+  } else if (Math.ceil(final) == 3 && gender === "female") {
+      document.getElementById("result").innerHTML = 
+       "You were born on Wednesday,Your Akan Day Name is" + femaleAkanName[3];
+  } else if (Math.ceil(final) == 4 && gender === "female") {
+      document.getElementById("result").innerHTML = 
+       "You were born on Thursday,Your Akan Day Name is" + femaleAkanName[4];
+  } else if (Math.ceil(final) == 5 && gender === "female") {
+     document.getElementById("result").innerHTML = 
+       "You were born on Friday,Your Akan Day Name is" + femaleAkanName[5];
+  } else if (Math.ceil(final) == 6 && gender === "female") {
+     document.getElementById("result").innerHTML = 
+       "You were born on Saturday,Your Akan Day Name is" + femaleAkanName[6];
+  } else {
+     alert("Enter all data,please");
+ }
+}
